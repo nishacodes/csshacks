1aee2d094f49c7fe87cb8924d5bd556cc3bc617e
nishacodes <nishabatra27@gmail.com>
2014-01-07 18:08:59 -0500

    create new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Csshacks::Application.config.secret_token = '08b332efd25a09a91efae021a65272b2288436582ee6b7713731125cb211a2c46dd4d29454b5c5daa69c58cbae74c48a6c548e5466f6d06e2c13cb26b3929553'
