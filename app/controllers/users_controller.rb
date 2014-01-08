d4c157d13bb839048eb58b6a2ef9ff9eaf331504
nishacodes <nishabatra27@gmail.com>
2014-01-08 17:46:26 -0500

    show creator of the hack on slide

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class Users::SessionsController < Devise::SessionsController
   
+  def show
+    @user = current_user
 
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
+  end
 end
