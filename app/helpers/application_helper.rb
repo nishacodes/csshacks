51e4fd93a6ee700c6ca4ac988e3e5d3b418d87e2
nishacodes <nishabatra27@gmail.com>
2014-01-29 14:48:25 -0500

    style hack navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,9 +12,5 @@ module ApplicationHelper
     @devise_mapping ||= Devise.mappings[:user]
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) } # The :firstname and :lastname are my custom fields.
-  end
 
-  # protect_from_forgery with: :exception
 end
